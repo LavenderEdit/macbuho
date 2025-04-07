54a73b8f9df0ca9cb41cb710425c1af6e8b82f70
Lavender <gercermagden@gmail.com>
2025-04-06 20:33:25 -0500

    refactor(scraping): mejora estructura del script get_json_apple.js

--- nodeJS/get_json_apple.js
+++ nodeJS/get_json_apple.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const UserAgent =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0";
 
-(async () => {
+async function scrapData() {
   const browser = await puppeteer.launch({
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
@@ -11,48 +11,39 @@ const UserAgent =
 
   const page = await browser.newPage();
   await page.setUserAgent(UserAgent);
+
   await page.goto("https://www.apple.com/shop/refurbished/mac");
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  const data = await page.evaluate(() =>
+  const macData = await page.evaluate(() =>
     JSON.stringify(window.REFURB_GRID_BOOTSTRAP)
   );
-
-  await browser.close();
-
   fs.writeFile(
     "C:/xampp/htdocs/macbuho/json/mac.json",
-    data,
+    macData,
     function (err) {
       if (err) return console.log(err);
       console.log("Datos de Mac extraídos correctamente.");
     }
   );
-})();
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-
-  const page = await browser.newPage();
-  await page.setUserAgent(UserAgent);
   await page.goto("https://www.apple.com/shop/refurbished/ipad");
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  const data = await page.evaluate(() =>
+  const ipadData = await page.evaluate(() =>
     JSON.stringify(window.REFURB_GRID_BOOTSTRAP)
   );
-
-  await browser.close();
-
   fs.writeFile(
     "C:/xampp/htdocs/macbuho/json/ipad.json",
-    data,
+    ipadData,
     function (err) {
       if (err) return console.log(err);
       console.log("Datos de iPad extraídos correctamente.");
     }
   );
-})();
+
+  await browser.close();
+}
+
+setInterval(scrapData, 15 * 60 * 1000); // 15 minutos
+// setInterval(scrapData, 3 * 60 * 60 * 1000);
